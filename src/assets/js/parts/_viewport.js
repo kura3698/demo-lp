9c8ed9f79fe200f20924a1c87c406d7378f5960e
HajimeKouno <kura.ai.3698@gmail.com>
2026-06-17 13:54:13 +0900

    ［#19］サファリ

--- src/assets/js/parts/_viewport.js
+++ src/assets/js/parts/_viewport.js
@@ -1,18 +1,18 @@
-// 390px以下で viewport を固定
-!(function () {
-  const viewport = document.querySelector('meta[name="viewport"]');
-  if (!viewport) return;
-
-  function switchViewport() {
-    const value =
-      window.outerWidth > 390
-        ? "width=device-width,initial-scale=1"
-        : "width=390";
-    if (viewport.getAttribute("content") !== value) {
-      viewport.setAttribute("content", value);
-    }
-  }
-
-  addEventListener("resize", switchViewport, false);
-  switchViewport();
-})();
+// 390px以下で viewport を固定
+!(function () {
+  const viewport = document.querySelector('meta[name="viewport"]');
+  if (!viewport) return;
+
+  function switchViewport() {
+    const value =
+      window.outerWidth > 390
+        ? "width=device-width,initial-scale=1"
+        : "width=390";
+    if (viewport.getAttribute("content") !== value) {
+      viewport.setAttribute("content", value);
+    }
+  }
+
+  addEventListener("resize", switchViewport, false);
+  switchViewport();
+})();
